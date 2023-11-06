d87a2940af46c33f967b47ce982b2d95de729a7e
mtamrod <trmarioiesrafaelalberti.es>
2023-11-06 12:02:19 +0100

    Subida de ejercicios

--- src/P2.2/ej2_2_10.py
+++ src/P2.2/ej2_2_10.py
@@ -1,24 +1,19 @@
-def primo_func(n):
-    for x in range(n, 2, -1):
-        primo=n%x
-        if primo == 0:
-            x-=1
-            primo=n%x
-            if primo == 0:
-                return print("No es primo")
-            #else:
-            #    return print("Es primo")
+def es_primo(n):
+    if n<=1 and n>=-1:
+        return print("No es primo")
+    
+    for i in range(2, n):
+        primo=n%i
+
+        if primo==0:
+            return print("No es primo")
         else:
-            return print("Es primo")
-
-
-        
-        
+            return print("Es primo")      
 
 def main():
     n=int(input("Introduzca un numero entero: "))
     
-    primo_func(n)
+    es_primo(n)
 
 if __name__ == "__main__":
     main()
