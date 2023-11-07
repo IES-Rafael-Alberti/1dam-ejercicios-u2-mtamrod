48c82ce87b017f5649b54d97b35b8ceb60a2f636
mtamrod <trmario@iesrafaelalberti.es>
2023-11-07 19:58:37 +0100

    a

--- src/P2.2/ej2_2_18.py
+++ src/P2.2/ej2_2_18.py
@@ -1,12 +1,18 @@
-def mayor_func(n, suma):
-    for digito in n:
-        suma+=int(digito)
-    return print(suma)
+def sumatorio(n):
+    suma2=0
+    while n!="-1":
+        suma=0
+        for digito in n:
+            suma+=int(digito)
 
+        suma2+=int(suma)
+        n=str(input("Introduce un numero: ")) 
+    return suma2
+    
 def main():
-    suma=0
-    n=str(input("Introduce un numero: "))
-    mayor_func(n, suma)
+    n=str(input("Introduce un numero: ")) 
+    
+    print(f"La suma es: {sumatorio(n)}")
 
 if __name__ == "__main__":
     main()
